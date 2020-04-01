7877b9ddca080ca214439fa2b71273d7cb3bf540
BrandonMGH <BrandonMGH@Yahoo.com>
2020-04-01 10:53:52 -0700

    moved handler.js file to Api directory and renamed file to services.js

--- Api/services.js
+++ Api/services.js
@@ -12,7 +12,17 @@ module.exports.hello = async event => {
       2
     ),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.goodbye = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Goodbye'
+      },
+      null,
+      2
+    ),
+  };
 };
